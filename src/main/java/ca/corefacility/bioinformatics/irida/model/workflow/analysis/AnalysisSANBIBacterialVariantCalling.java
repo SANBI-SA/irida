4f5b6f80668ab7ff23f9c4baa3537306bcaae5df
Zipho Mashologu <zipho.m@gmail.com>
2017-07-23 22:24:14 +0200

    workflow analysis definition

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/workflow/analysis/AnalysisSANBIBacterialVariantCalling.java
@@ -0,0 +1,36 @@
+package ca.corefacility.bioinformatics.irida.model.workflow.analysis;
+
+import java.util.Map;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * Metadata for SANBI Bacterial Variant Calling.
+ * 
+ *
+ */
+@Entity
+@Table(name = "analysis_sanbi_bacterial_variant_calling")
+public class AnalysisSANBIBacterialVariantCalling extends Analysis {
+
+	/**
+	 * required for hibernate, marked as private so nobody else uses it.
+	 */
+	@SuppressWarnings("unused")
+	private AnalysisSANBIBacterialVariantCalling() {
+		super();
+	}
+
+	public AnalysisSANBIBacterialVariantCalling(final String executionManagerAnalysisId,
+			final Map<String, AnalysisOutputFile> analysisOutputFilesMap) {
+		super(executionManagerAnalysisId, analysisOutputFilesMap);
+	}
+
+	@JsonIgnore
+	public AnalysisOutputFile getSISTRResults() {
+		return getAnalysisOutputFile("sistr-predictions");
+	}
+}
